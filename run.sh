68393f8e8c87914aaec18f789cec9ecddc05b571
sebastian@lapela.dk <sebastian@lapela.dk>
2016-01-11 21:26:14 +0000

    now it is working

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~/st
-sudo docker build -t seeeb/streesmind .
-docker run --name stresmind -d -p 80:3000 seeeb/stressmind
+sudo docker build -t seeeb/stressmind .
+#docker stop stressmind
+docker rm stressmind
+docker run --name stressmind -d -p 80:3000 seeeb/stressmind
