34fec48e8631ed615ca5d72141decce4cfc5233a
seastian <sebastian@momentos.dk>
2016-01-11 20:13:24 +0100

    fixing dockerfile and run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,4 @@
 cd ~/st
 sudo docker build -t seeeb/streesmind .
 
-docker run --name stresmind -it -e VIRTUAL_HOST=stress.lapela.dk -v ~/st:/usr/src/app \
-    -e APP_NAME=app \
-    -e APP_REPO_URL="https://github.com/sinise/st.git" \
-    -e RAILS_ENV=production \
-    -e DATABASE_URL="mongo://hao:hao@paulo.mongohq.com:10045/streemind_test" \
-    -e SECRET_KEY_BASE="etellerandetsecret"
-    zumbrunnen/rails
+docker run --name stresmind -d -e VIRTUAL_HOST=stress.lapela.dk -e VIRTUAL_SSL_HOST=stress.lapela.dk -v ~/st:/usr/src/app
